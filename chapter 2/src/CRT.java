c5977ddbb152d4e79dc9394768d799b7b353e9f6
1100054549 <1100054549@L815WD220024.edu.cbe.ab.ca>
2022-09-13 09:56:58 -0600

    10:00 am

---
+++ chapter 2/src/CRT.java
@@ -0,0 +1,31 @@
+
+public class CRT
+{
+
+	public static void main(String[] args)
+	{
+		
+	/*
+		 3.
+		 /**
+		 Average.java
+		 Calculate test averages
+		 Emmy
+		 September, 13, 2022 
+		  
+		 	
+		4.//The AverageScore class displays the average of a set of scores.
+		
+		5.//package gradeCalculator;
+		
+		6.//public class AverageScore
+		
+		7.//A source code is the code typed by the programmer. A byte code is when the source code is compiled.
+		
+		8.//Print() is a method that displays output to the screen. Println() is a method that displays output to the screen.
+	
+	*/
+				
+	}
+
+}
