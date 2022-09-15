e3bf85d259a4108aa59a6ea53aa8e7f7e52a1e0c
1100054549 <1100054549@L815WD220024.edu.cbe.ab.ca>
2022-09-15 09:32:28 -0600

    9:30 am

--- chapter 3/src/skillbuilding/GradeAvgP1.java
+++ chapter 3/src/skillbuilding/GradeAvgP1.java
@@ -1,9 +1,33 @@
 package skillbuilding;
 
-public class GradeAvgP1 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class GradeAvgP1
+{
+
+	public static void main(String[] args)
+	{
+		
+		Scanner prompt = new Scanner(System.in);
+		
+		System.out.println("Please enter the first grade:");
+		int first = prompt.nextInt();
+		
+		System.out.println("Please enter the second grade:");
+		int second = prompt.nextInt();
+		
+		System.out.println("Please enter the third grade:");
+		int third = prompt.nextInt();
+		
+		System.out.println("Please enter the fourth grade:");
+		int fourth = prompt.nextInt();
+		
+		System.out.println("Please enter the fifth grade:");
+		int fifth = prompt.nextInt();
+		
+		double x = (Math.round(first + second + third + fourth + fifth)/5);
+		
+		System.out.print("Average:" + x);
 
 	}
 
---
+++ chapter 3/src/skillbuilding/GradeAvgP2.java
@@ -0,0 +1,13 @@
+package skillbuilding;
+
+public class GradeAvgP2
+{
+
+	public static void main(String[] args)
+	{
+		
+		
+		
+	}
+
+}
