c0e64a31a1effdf3a81a084ba785af4959609a19
1100054549 <1100054549@L815WD220024.edu.cbe.ab.ca>
2022-09-15 09:32:39 -0600

    9:30 am

--- chapter 3/src/skillbuilding/GradeAvgP1.java
+++ chapter 3/src/skillbuilding/GradeAvgP1.java
@@ -10,24 +10,24 @@ public class GradeAvgP1
 		
 		Scanner prompt = new Scanner(System.in);
 		
-		System.out.println("Please enter the first grade:");
+		System.out.println("Please enter the first grade: ");
 		int first = prompt.nextInt();
 		
-		System.out.println("Please enter the second grade:");
+		System.out.println("Please enter the second grade: ");
 		int second = prompt.nextInt();
 		
-		System.out.println("Please enter the third grade:");
+		System.out.println("Please enter the third grade: ");
 		int third = prompt.nextInt();
 		
-		System.out.println("Please enter the fourth grade:");
+		System.out.println("Please enter the fourth grade: ");
 		int fourth = prompt.nextInt();
 		
-		System.out.println("Please enter the fifth grade:");
+		System.out.println("Please enter the fifth grade: ");
 		int fifth = prompt.nextInt();
 		
 		double x = (Math.round(first + second + third + fourth + fifth)/5);
 		
-		System.out.print("Average:" + x);
+		System.out.print("Average: " + x);
 
 	}
 
