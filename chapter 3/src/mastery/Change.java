5db97dbb96442ab59ca5c3fdde122141d8f03628
1100054549 <1100054549@L815WD220024.edu.cbe.ab.ca>
2022-09-19 12:27:53 -0600

    12:27 pm

--- chapter 3/src/mastery/Change.java
+++ chapter 3/src/mastery/Change.java
@@ -9,7 +9,7 @@ public class Change {
 		
 		Scanner prompt = new Scanner(System.in);
 		
-		System.out.println("Please enter an amount less than 100 cents: ");
+		System.out.println("Enter the change in cents: ");
 		int change = prompt.nextInt();
 		
 		if  (change>100)
@@ -20,6 +20,13 @@ public class Change {
 		}
 		else
 		{
+			
+			int quarter = (int)Math.floor(change/25);
+			int dime = ((change%25)/10);
+			int nickel = ((change%25%10)/5);
+			int penny = ((change%25%10%5)/1);
+			System.out.println
+			("The minimum number of coins is:\nQuarters: " + quarter + "\nDimes: " + dime + "\nNickels: " + nickel + "\nPennies: " + penny);	
 			
 		}
 
--- chapter 3/src/skillbuilding/Digits.java
+++ chapter 3/src/skillbuilding/Digits.java
@@ -1,5 +1,31 @@
 package skillbuilding;
 
-public class Digits {
+import java.util.Scanner;
+
+public class Digits
+{
+
+	public static void main(String[] args)
+	{
+		Scanner prompt = new Scanner(System.in);
+		
+		System.out.print("Please enter a two-digits number: ");
+		int n = prompt.nextInt();
+		
+		if (n>99)
+		{
+			
+			System.out.print("Error. You must enter a two-digits number.");
+			
+		}
+		else
+		{
+			
+		System.out.println("Tens-place digit: " + (n/10));
+		System.out.print("Ones-place digit: " +  (n%10));
+		
+		}
+		
+	}
 
 }
