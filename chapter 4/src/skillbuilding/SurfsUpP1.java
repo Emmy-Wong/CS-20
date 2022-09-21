589132c2328953e658dfff7376ac8e18a5dba9c2
1100054549 <1100054549@L815WD220024.edu.cbe.ab.ca>
2022-09-21 12:01:17 -0600

    12:01 pm

--- chapter 4/src/skillbuilding/SurfsUpP1.java
+++ chapter 4/src/skillbuilding/SurfsUpP1.java
@@ -1,10 +1,30 @@
+/*
+
+Program: SurfsUpP1.java          Last Date of this Revision: September 30, 2019
+
+Purpose: Create a SurfsUp application that prompts the user for the wave height and then displays "Great day for surfing!" when the waves are 6 feet and over.
+
+Author: Emmy Wong, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package skillbuilding;
 
-public class SurfsUpP1 {
+public class SurfsUpP1
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 }
+/* Screen Dump
+
+Paste the output of your code here
+ 
+ */
